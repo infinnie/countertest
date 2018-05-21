d0792985a63ab5da53ec52b2523878b4e394e83a
joan xie <w4ergf@qq.com>
2018-05-21 18:00:40 +0800

    Gulp 4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,15 @@ var gulp = require("gulp"),
     crypto = require("crypto"),
     fileLookUp = {},
     destPath = "build/dist",
+    path = require("path"),
+    pathBase = path.resolve() + path.sep,
     addHash = function (original) {
         var t = new Transform({ objectMode: true });
         t._transform = function (file, encoding, callback) {
-            // console.dir(Object.keys(file._contents));
             var hash = crypto.createHash("sha256"),
                 history = file.history,
                 curHistory = history[history.length - 1],
                 hashedFileName;
-            //console.log(file._contents);
             hash.update(file._contents);
             hashedFileName = curHistory.replace(/(\.[^.]+)$/, "-" + hash.digest("hex").slice(0, 8) + "$1");
             original = original || curHistory;
@@ -24,6 +24,7 @@ var gulp = require("gulp"),
         };
         return t;
     };
+
 gulp.task("thing", function () {
     return gulp.src("jsx/**/*.jsx").pipe(babel()).pipe(gulp.dest("build"));
 });
@@ -32,36 +33,35 @@ gulp.task("build:define", function () {
     return gulp.src("define.js").pipe(uglify({ ie8: true })).pipe(addHash()).pipe(gulp.dest(destPath));
 });
 
-gulp.task("build:main", ["thing"], function () {
+gulp.task("build:main", gulp.series("thing", function () {
     return gulp.src(["lib/h.js", "lib/app.js", "build/main.js"])
         .pipe(concat("main-bundle.js"))
         .pipe(uglify({ ie8: true }))
         .pipe(addHash("jsx/main.jsx"))
         .pipe(gulp.dest(destPath));
-});
+}));
 
-gulp.task("build:counter", ["thing"], function () {
+gulp.task("build:counter", gulp.series("thing", function () {
     return gulp.src(["js/counterfunctions.js", "js/connect.js", "build/counter.js"])
         .pipe(concat("counter-bundle.js"))
         .pipe(uglify({ ie8: true }))
         .pipe(addHash("jsx/counter.jsx"))
         .pipe(gulp.dest(destPath));
-});
+}));
 
-gulp.task("build", ["build:define", "build:main", "build:counter"], function () {
+gulp.task("build", gulp.series(gulp.parallel("build:define", "build:main", "build:counter"), function () {
     return gulp.src("index.src.html").pipe(function () {
         var t = new Transform({ objectMode: true });
         t._transform = function (file, encoding, callback) {
             var fileContent = file._contents.toString(encoding);
             Object.keys(fileLookUp).forEach(function (x) {
-                console.log(x);
-                var y = x.replace(file.base, "").replace(/[\\/]/g, "\\/").replace(/\./g, "\\.");
-                console.log(fileLookUp[x]);
+                // make it suitable for conversion to regular expressions
+                var y = x.replace(pathBase, "").replace(/[\\/]/g, "\\/").replace(/\./g, "\\.");
                 fileContent = fileContent.replace(new RegExp("%" + y), fileLookUp[x].replace(/^.*?\\([^\\]+)$/, "/" + destPath + "/$1"));
             });
             file._contents = Buffer.from(fileContent);
             callback(null, file);
         };
         return t;
-    }()).pipe(concat("index.html")).pipe(gulp.dest(""));
-});
+    }()).pipe(concat("index.html")).pipe(gulp.dest("."));
+}));
